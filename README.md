c76eab3251f77ef831dd7b75bff6095689f8d7a9
jeffrey-chr <jeffrey.t.chr@gmail.com>
2021-06-24 22:52:28 +1000

    Add QAP file writer to utilities, update submodules.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ISA-QAP
 Instance space analysis of the Quadratic Assignment Problem
+
+You don't have to clone this repository; if you clone the submodule repositories, copy the "Utilities" folder, 
+and set up a few config files then everything will work fine. 
+
+If you do clone this repository:
+
+Step 1 - Get whatever submodules you need.
+
+InstanceGeneration and Features are dependent on the Utilities folder.
+Algorithms will compile on its own but to do anything useful with it you will require some QAP instance data files like those in the Instances folder.
+
+Step 2 - Run configure.bat (Windows) or configure.bash (Linux) to set up the config files automatically.
---
+++ Utilities/qap_writeFile.m
@@ -0,0 +1,34 @@
+function qap_writeFile(filename,dist,flow)
+%QAP_WRITEFILE Writes QAP instance to file.
+
+n = size(dist,1);
+
+fid = fopen(filename,'w');
+
+% Finds the maximum width required for each matrix column
+% in the data file to make everything align nicely.
+digits = floor(max(max(log10(dist(:))),max(log10(flow(:)))))+1;
+
+fprintf(fid,"%d\n\n",n);
+
+for i = 1:n
+    for j = 1:n
+        fprintf(fid,strcat("%",num2str(digits),"d  "),dist(i,j));
+    end
+    fprintf(fid,"\n");
+end
+
+fprintf(fid,"\n");
+
+for i = 1:n
+    for j = 1:n
+        fprintf(fid,strcat("%",num2str(digits),"d  "),flow(i,j));
+    end
+    fprintf(fid,"\n");
+end
+
+
+fclose(fid);
+
+end
+

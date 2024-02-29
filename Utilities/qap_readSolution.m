dee9ca610e3538c7b87ce6b53596ef254800f6b9
jeffrey-chr <jeffrey.t.chr@gmail.com>
2024-02-29 17:06:03 +1100

    Update modules and implement readSolution

--- Utilities/qap_readSolution.m
+++ Utilities/qap_readSolution.m
@@ -1,7 +1,24 @@
-function [outputArg1,outputArg2] = qap_readSolution(inputArg1,inputArg2)
-%QAP_READSOLUTION Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [val,perm] = qap_readSolution(filename)
+% qap_readFile Reads a QAP solution from a file.
+% Solutions are assumed to be of the form:
+% n val
+% perm
+% where perm is a vector of length n, possibly spread over multiple lines, 
+% representing the solution permutation.
+fid = fopen(filename);
+[A,count] = fscanf(fid,'%d');
+fclose(fid);
+
+n = A(1);
+if (count ~= n+2) 
+    error(strcat("invalid slnfile ",filename));
+end
+
+val = A(2);
+perm = A(3:end);
+if any(perm == 0) % a few solution files start at 0 instead.
+    perm = perm + 1;
+end
+
 end
 
